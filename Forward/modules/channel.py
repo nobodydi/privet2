b516acaecfaac0d19350f978e2e09f6b2552ec4f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-17 00:33:10 +0530

    Update channel.py

--- Forward/modules/channel.py
+++ Forward/modules/channel.py
@@ -13,7 +13,7 @@ async def media(_, message):
 
 """
 
-
+x = ["S01","S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "season 01","season 02","season 03","season 04","season 05","season 06","season 07","season 08","season 09","season 10", "season 1","season 2","season 3","season 4","season 5","season 6","season 7","season 8","season 9"]
 # -------------------» ᴄʜᴀɴɴᴇʟ «------------------- #
 
 media_filter = filters.document | filters.video | filters.audio
@@ -24,10 +24,11 @@ async def media(_, message):
     file_name = message.document.file_name if message.document else \
                 message.video.file_name if message.video else \
                 message.audio.file_name if message.audio else None
-    
-    if file_name and any(keyword in file_name.lower() for keyword in ["SO1"]):
-        await app.copy_message(chat_id=OTHER_ID, from_chat_id=FORWARD_IDS, message_id=message.id)
-    else:
-        await app.copy_message(chat_id=CHANNEL_ID, from_chat_id=FORWARD_IDS, message_id=message.id)
+    print(file_name)
+    for i in list(file_name.lower()):
+        if i in x.lower():
+            await app.copy_message(chat_id=OTHER_ID, from_chat_id=FORWARD_IDS, message_id=message.id)
+        else:
+            await app.copy_message(chat_id=CHANNEL_ID, from_chat_id=FORWARD_IDS, message_id=message.id)
 
 
