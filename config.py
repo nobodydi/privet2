3a0dbc3bf4e71e473fe0de562a72a95e05aa1897
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-16 23:21:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "6315381025:AAHQbodUGYQ2r6q1Fi0cjbeFelhn_4s2zAY")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280103226").split()))
+
+# --------------- Channel ------------ #
+FORWARD_ID = []
+CHANNEL_ID = []
