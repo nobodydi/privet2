9d9b95b4f08a55c8c29047a58f583e556c9b807b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-16 23:29:26 +0530

    Update channel.py

--- Forward/modules/channel.py
+++ Forward/modules/channel.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from Hancock.core.utils import save_file
-from vars import CHANNEL_IDS
+from config import CHANNEL_ID, FORWARD_IDS
 from Hancock import app
 
 # -------------------» ᴄʜᴀɴɴᴇʟ «------------------- #
