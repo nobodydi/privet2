f526569a52306d69d742d37f358ddeb507a53ad5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-16 23:43:29 +0530

    Update channel.py

--- Forward/modules/channel.py
+++ Forward/modules/channel.py
@@ -1,6 +1,18 @@
 from pyrogram import filters
-from config import CHANNEL_ID, FORWARD_IDS
+from config import CHANNEL_ID, FORWARD_IDS, OTHER_ID
 from Forward import app
+
+# -------------------» ᴄʜᴀɴɴᴇʟ «------------------- #
+"""
+media_filter = filters.document | filters.video | filters.audio
+
+
+@app.on_message(filters.chat(FORWARD_IDS) & media_filter)
+async def media(_, message):
+    await app.copy_message(chat_id=CHANNEL_ID, from_chat_id=FORWARD_IDS, message_id=message.id)
+
+"""
+
 
 # -------------------» ᴄʜᴀɴɴᴇʟ «------------------- #
 
@@ -9,5 +21,13 @@ media_filter = filters.document | filters.video | filters.audio
 
 @app.on_message(filters.chat(FORWARD_IDS) & media_filter)
 async def media(_, message):
-    await app.copy_message(chat_id=CHANNEL_ID, from_chat_id=FORWARD_IDS, message_id=message.id)
+    file_name = message.document.file_name if message.document else \
+                message.video.file_name if message.video else \
+                message.audio.file_name if message.audio else None
+    
+    if file_name and any(keyword in file_name.lower() for keyword in ["SO1"]):
+        await app.copy_message(chat_id=OTHER_ID, from_chat_id=FORWARD_IDS, message_id=message.message_id)
+    else:
+        await app.copy_message(chat_id=CHANNEL_ID, from_chat_id=FORWARD_IDS, message_id=message.message_id)
+
 
