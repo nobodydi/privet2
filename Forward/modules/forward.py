3dcd41155f22954ec89eaf28b27c7f52f8442ead
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-17 15:39:42 +0530

    Update forward.py

--- Forward/modules/forward.py
+++ Forward/modules/forward.py
@@ -25,29 +25,31 @@ async def batch(client, message):
         
         try:
             await message.reply_text("file transfering...")
-            i = 0
-            for i in range(int(last_msg_id)):
-                lol = await client.get_messages(chat_id, i)
+            msg_id = 0
+            for msg_id in range(int(last_msg_id)):
+                lol = await client.get_messages(chat_id, msg_id)
                 if "document" in lol:
                     file_name = lol.docoument.file_name
                 elif "video" in lol:   
                     file_name = lol.video.file_name
                 print(file_name)
-                print(f"message id: {i}")
+                print(f"message id: {msg_id}")
                 for keyword in x:
                     if keyword in file_name:
                         try:
-                            await app.copy_message(chat_id=SERIES_ID, from_chat_id=FORWARD_IDS, message_id=i)
+                            await app.copy_message(chat_id=SERIES_ID, from_chat_id=FORWARD_IDS, message_id=msg_id)
                         except Exception as e:
                             print(f"Failed to copy message {i}: {e}")
                             continue
                         break
                 else:
                     try:
-                        await app.copy_message(chat_id=MOVIES_ID, from_chat_id=FORWARD_IDS, message_id=i)
+                        await app.copy_message(chat_id=MOVIES_ID, from_chat_id=FORWARD_IDS, message_id=msg_id)
                     except Exception as e:
                         print(f"Failed to copy message {i}: {e}")
                         continue
+
+            await message.reply_text(msg_id)
 
         except Exception as e:
             print(e)
