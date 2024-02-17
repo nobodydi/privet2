71f06eb55f9a2391fedd35e7d9658d87d811498c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-17 20:36:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from os import getenv
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "6315381025:AAHQbodUGYQ2r6q1Fi0cjbeFelhn_4s2zAY")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280103226").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280103226 5416887843").split()))
 
 # --------------- Channel ------------ #
 FORWARD_IDS = -1001652627420
-MOVIES_ID = -1002045076727
-SERIES_ID = -1002142358903
+MOVIES_ID = -1001945676856
+SERIES_ID = -1001991706703
