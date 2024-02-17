33451ea234ce248ca51384976dfbe866a0d82d6e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-17 16:06:14 +0530

    Update forward.py

--- Forward/modules/forward.py
+++ Forward/modules/forward.py
@@ -15,24 +15,28 @@ async def batch(client, message):
     if lock.locked():
         await message.reply("<code>Wait until previous process completes.</code>")
     else:
-        last_msg = await client.ask(text="<code>Forward last message from DB channel (with quotes)\n\nor send the DB channel post link</code>", chat_id=message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)        
         try:
+            last_msg = await client.ask(
+                text="<code>Forward last message from DB channel (with quotes)\n\nor send the DB channel post link</code>",
+                chat_id=message.from_user.id,
+                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+                timeout=60
+            )
             last_msg_id = last_msg.forward_from_message_id
             chat_id = last_msg.forward_from_chat.username or last_msg.forward_from_chat.id
-            lol = await client.get_messages(chat_id, last_msg_id)
-            
         except Exception as e:
             await last_msg.reply_text(f"Error: {e}")
+            return
         
         try:
-            await message.reply_text("file transfering...")
-            
+            await message.reply_text("file transferring...")
             total_ids = [i for i in range(int(last_msg_id) + 1)]
             for msg_id in total_ids:
                 await asyncio.sleep(0.5)
                 lol = await client.get_messages(chat_id, msg_id)
+                file_name = None
                 if "document" in lol:
-                    file_name = lol.docoument.file_name
+                    file_name = lol.document.file_name
                 elif "video" in lol:   
                     file_name = lol.video.file_name
                 print(file_name)
@@ -42,17 +46,15 @@ async def batch(client, message):
                         try:
                             await app.copy_message(chat_id=SERIES_ID, from_chat_id=FORWARD_IDS, message_id=msg_id)
                         except Exception as e:
-                            print(f"Failed to copy message {i}: {e}")
-                            continue
+                            print(f"Failed to copy message {msg_id}: {e}")
                         break
                 else:
                     try:
                         await app.copy_message(chat_id=MOVIES_ID, from_chat_id=FORWARD_IDS, message_id=msg_id)
                     except Exception as e:
-                        print(f"Failed to copy message {i}: {e}")
-                        continue
+                        print(f"Failed to copy message {msg_id}: {e}")
 
-            await message.reply_text(msg_id)
-
+            await message.reply_text("Completed copying messages.")
         except Exception as e:
             print(e)
+
