e45f3c768b17e406ada4b7784989004d16b7a62e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-17 18:57:39 +0530

    Update forward.py

--- Forward/modules/forward.py
+++ Forward/modules/forward.py
@@ -10,6 +10,24 @@ from Forward.modules.channel import x
 lock = asyncio.Lock()
 
 
+
+async def file_finder(app, chat_id, msg_id):
+    lol = await app.get_messages(chat_id, msg_id)
+    file_name = None
+    if lol:
+        if lol.document:
+            file_name = lol.document.file_name
+            return file_name
+        elif lol.video:
+            file_name = lol.video.file_name
+            return file_name
+        else:
+            return "No media found in the message."
+    else:
+        return "No message found with the given ID."
+
+
+
 @app.on_message(filters.command("index") & filters.user(SUDO_USERS))
 async def batch(client, message):
     if lock.locked():
@@ -29,32 +47,41 @@ async def batch(client, message):
             return
         
         try:
-            await message.reply_text("file transferring...")
-            total_ids = [i for i in range(int(last_msg_id) + 1)]
-            for msg_id in total_ids:
-                await asyncio.sleep(0.5)
-                lol = await client.get_messages(chat_id, msg_id)
-                file_name = None
-                if "document" in lol:
-                    file_name = lol.document.file_name
-                elif "video" in lol:   
-                    file_name = lol.video.file_name
+            message_ids = [i for i in range(int(last_msg_id) + 1)]
+            await asyncio.sleep(0.5)
+            for message_id in message_ids:
+    
+                file_name = await file_finder(app, FORWARD_IDS, message_id)
                 print(file_name)
-                print(f"message id: {msg_id}")
                 for keyword in x:
+                    await asyncio.sleep(0.5)
                     if keyword in file_name:
                         try:
-                            await app.copy_message(chat_id=SERIES_ID, from_chat_id=FORWARD_IDS, message_id=msg_id)
+                            await asyncio.sleep(1)
+                            await app.copy_message(chat_id=SERIES_ID, from_chat_id=FORWARD_IDS, message_id=message_id)
+                            break
                         except Exception as e:
                             print(f"Failed to copy message {msg_id}: {e}")
-                        break
+                            continue
                 else:
                     try:
-                        await app.copy_message(chat_id=MOVIES_ID, from_chat_id=FORWARD_IDS, message_id=msg_id)
+                        await asyncio.sleep(1)
+                        await app.copy_message(chat_id=MOVIES_ID, from_chat_id=FORWARD_IDS, message_id=message_id)
                     except Exception as e:
-                        print(f"Failed to copy message {msg_id}: {e}")
-
-            await message.reply_text("Completed copying messages.")
+                        print(f"Failed {message_id}: {e}")
+                        continue           
         except Exception as e:
-            print(e)
+            print(f"Error: {e}")
+
+
+          
+
+
+
+
+
+
+
+                
+
 
