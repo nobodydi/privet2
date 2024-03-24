133cce8756d376a089887bdb5abae19c6e9acfbe
Mr BROLI <149184961+nobodydi@users.noreply.github.com>
2024-03-24 08:47:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6917556221:AAEcDrW1CWI1-IHqphbutfaSoaCiwdtd1nQ"
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6842001658").split()))
 
 # --------------- Channel ------------ #
-FORWARD_IDS = -1002032823792
+FORWARD_IDS = -1001906675829
 MOVIES_ID = -1002017670110
 SERIES_ID = -1002092615913
