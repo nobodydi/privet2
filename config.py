2f8ebd7b7130b86070c44213c979635ce0453618
Mr BROLI <149184961+nobodydi@users.noreply.github.com>
2024-03-24 08:54:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "6917556221:AAEcDrW1CWI1-IHqphbutfaSoaCiwdtd1nQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "6917556221:AAEzXZE5N9dbrHaKRLnatkuNiyFkpp1ajDo")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6842001658").split()))
 
 # --------------- Channel ------------ #
-FORWARD_IDS = -1001906675829
+FORWARD_IDS = -1002032823792
 MOVIES_ID = -1002017670110
 SERIES_ID = -1002092615913
